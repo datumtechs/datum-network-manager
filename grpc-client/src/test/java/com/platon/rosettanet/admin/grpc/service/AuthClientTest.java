5d9c95b25c24a10e703f84b6716924b0b178122d
liushuyu <754053007@qq.com>
2021-07-08 18:53:34 +0800

    grpc demo

--- grpc-client/src/test/java/com/platon/rosettanet/admin/grpc/service/AuthClientTest.java
+++ grpc-client/src/test/java/com/platon/rosettanet/admin/grpc/service/AuthClientTest.java
@@ -1,8 +1,14 @@
 package com.platon.rosettanet.admin.grpc.service;
 
+import com.platon.rosettanet.admin.grpc.client.AuthClient;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import io.grpc.stub.StreamObserver;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 
 /**
@@ -14,6 +20,25 @@ import org.junit.Test;
 public class AuthClientTest {
 
 
+    /**
+     * 启动一个服务
+     * @throws Exception
+     */
+    @Before
+    public void startService() throws Exception {
+        /* The port on which the server should run */
+        int port = 50051;
+        //这个部分启动server
+        NettyServerBuilder.forPort(port)
+                .addService(new AuthServiceImpl().bindService())
+                .build()
+                .start();
+        System.out.println("Server started, listening on " + port);
+    }
+
+    /**
+     * 测试客户端
+     */
     @Test
     public void test() {
         Channel channel =  ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
@@ -22,5 +47,20 @@ public class AuthClientTest {
         CommonMessage.SimpleResponseCode responseCode = AuthServiceGrpc.newBlockingStub(channel).applyIdentityJoin(joinRequest);
         System.out.println("###############" + responseCode.getMsg());
         System.out.println("111111111");
+    }
+
+
+    /**
+     * 服务端实现
+     */
+    static class AuthServiceImpl extends AuthServiceGrpc.AuthServiceImplBase {
+
+        @Override
+        public void applyIdentityJoin(AuthRpcMessage.ApplyIdentityJoinRequest request, StreamObserver<CommonMessage.SimpleResponseCode> responseObserver) {
+            System.out.println(request);
+            CommonMessage.SimpleResponseCode code = CommonMessage.SimpleResponseCode.newBuilder().setMsg("goooooooooooo").setStatus(0).build();
+            responseObserver.onNext(code);
+            responseObserver.onCompleted();
+        }
     }
 }
