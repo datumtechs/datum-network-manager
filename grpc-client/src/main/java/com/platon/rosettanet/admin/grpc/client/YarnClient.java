4a58ca510ad8f6955df108bd07a0db4c2b1eced6
liushuyu <754053007@qq.com>
2021-07-09 18:41:53 +0800

    新增获取可用的数据节点信息rpc客户端逻辑

--- grpc-client/src/main/java/com/platon/rosettanet/admin/grpc/client/YarnClient.java
+++ grpc-client/src/main/java/com/platon/rosettanet/admin/grpc/client/YarnClient.java
@@ -1,6 +1,16 @@
 package com.platon.rosettanet.admin.grpc.client;
 
+import cn.hutool.core.util.StrUtil;
+import com.platon.rosettanet.admin.common.exception.ApplicationException;
+import com.platon.rosettanet.admin.grpc.channel.BaseChannelManager;
+import com.platon.rosettanet.admin.grpc.entity.AvailableDataNodeResp;
+import com.platon.rosettanet.admin.grpc.service.YarnRpcMessage;
+import com.platon.rosettanet.admin.grpc.service.YarnServiceGrpc;
+import io.grpc.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * @Author liushuyu
@@ -12,5 +22,37 @@ import org.springframework.stereotype.Component;
  */
 
 @Component
+@Slf4j
 public class YarnClient {
+
+    @Resource(name = "simpleChannelManager")
+    private BaseChannelManager channelManager;
+
+    /**
+     * 根据需要上传的文件大小和类型，获取可用的数据节点信息
+     * @param scheduleServerHost 调度服务的ip
+     * @param scheduleServerPort 调度服务的port
+     */
+    public AvailableDataNodeResp getAvailableDataNode(String scheduleServerHost, int scheduleServerPort){
+        //1.获取rpc连接
+        Channel channel = channelManager.buildChannel(scheduleServerHost, scheduleServerPort);
+        //2.拼装request
+        YarnRpcMessage.QueryAvailableDataNodeRequest request = YarnRpcMessage.QueryAvailableDataNodeRequest.newBuilder().build();
+        //3.调用rpc,获取response
+        YarnRpcMessage.QueryAvailableDataNodeResponse response = YarnServiceGrpc.newBlockingStub(channel).queryAvailableDataNode(request);
+        //4.处理response
+        if(StrUtil.isBlank(response.getIp()) || StrUtil.isBlank(response.getPort())){
+            throw new ApplicationException(StrUtil.format("获取可用数据节点信息失败：ip:{},port:{}",
+                    response.getIp(),
+                    response.getPort()));
+        }
+        /**
+         * 由于调度服务rpc接口也在开发阶段，如果直接返回调度服务的response，一旦response发生变化，则调用该方法的地方都需要修改
+         * 故将response转换后再放给service类使用
+         */
+        AvailableDataNodeResp node = new AvailableDataNodeResp();
+        node.setIp(response.getIp());
+        node.setPort(Integer.parseInt(response.getPort()));
+        return node;
+    }
 }
