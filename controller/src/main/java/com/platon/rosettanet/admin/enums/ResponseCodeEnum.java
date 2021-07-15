a7dd263f4efee1a96970f91fdb81b618fa43de1d
liushuyu <754053007@qq.com>
2021-07-15 10:54:08 +0800

    修订错误码

--- controller/src/main/java/com/platon/rosettanet/admin/enums/ResponseCodeEnum.java
+++ controller/src/main/java/com/platon/rosettanet/admin/enums/ResponseCodeEnum.java
@@ -9,25 +9,11 @@ import lombok.Getter;
  * 500
  * 通用异常
  * 通用异常，包含参数无效、缺失、系统运行时异常等情况
- *
- *
- * 4001
- * 系统异常
- * 服务内部系统异常
- *
- *
- * 3100
- * 签名验证失败
- * 请检查签名规则是否合法
  */
 public enum ResponseCodeEnum {
 
     SUCCESS(0,"成功"),
     FAIL(500,"失败"),
-
-    SIGN_VALID_ERROR(3100,"签名验证失败"),
-    SYSTEM_ERROR(4001,"系统异常"),
-
     ;
 
     ResponseCodeEnum(int code, String message){
