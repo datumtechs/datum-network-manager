744f0abd1a111b426dc5a6449e2417f8050bf6f9
侯壮 <15736969819@163.com>
2021-07-22 10:45:29 +0800

    计算资源历史记录修改

---
+++ service/src/main/java/com/platon/rosettanet/admin/service/constant/ServiceConstant.java
@@ -0,0 +1,24 @@
+package com.platon.rosettanet.admin.service.constant;
+
+/**
+ * @author houz
+ * 实现模块常量类
+ */
+public class ServiceConstant {
+
+    public static final String constant_1 = "1";
+    public static final String constant_2 = "2";
+    public static final String constant_3 = "3";
+    public static final String constant_4 = "4";
+    public static final String constant_5 = "5";
+    public static final String constant_6 = "6";
+    public static final String constant_7 = "7";
+    public static final String constant_8 = "8";
+    public static final String constant_9 = "9";
+    public static final String constant_10 = "10";
+    public static final String constant_15 = "15";
+    public static final String constant_30 = "30";
+
+
+
+}
