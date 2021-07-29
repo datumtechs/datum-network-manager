a0fa227aaaf22406d81b95ad1361ddf138fccaf1
liushuyu <754053007@qq.com>
2021-07-29 12:08:39 +0800

    修改组织状态任务

--- service/src/main/java/com/platon/rosettanet/admin/service/task/LocalOrgRefreshTask.java
+++ service/src/main/java/com/platon/rosettanet/admin/service/task/LocalOrgRefreshTask.java
@@ -2,32 +2,18 @@ package com.platon.rosettanet.admin.service.task;
 
 import com.platon.rosettanet.admin.common.context.LocalOrgCache;
 import com.platon.rosettanet.admin.common.context.LocalOrgIdentityCache;
-import com.platon.rosettanet.admin.common.exception.ApplicationException;
-import com.platon.rosettanet.admin.common.util.BatchExecuteUtil;
-import com.platon.rosettanet.admin.dao.GlobalDataFileMapper;
-import com.platon.rosettanet.admin.dao.GlobalMetaDataColumnMapper;
 import com.platon.rosettanet.admin.dao.LocalOrgMapper;
-import com.platon.rosettanet.admin.dao.entity.GlobalDataFile;
-import com.platon.rosettanet.admin.dao.entity.GlobalDataFileDetail;
-import com.platon.rosettanet.admin.dao.entity.GlobalMetaDataColumn;
 import com.platon.rosettanet.admin.dao.entity.LocalOrg;
+import com.platon.rosettanet.admin.dao.enums.CarrierConnStatusEnum;
 import com.platon.rosettanet.admin.dao.enums.LocalOrgStatusEnum;
-import com.platon.rosettanet.admin.grpc.client.MetaDataClient;
 import com.platon.rosettanet.admin.grpc.client.YarnClient;
 import com.platon.rosettanet.admin.grpc.entity.YarnGetNodeInfoResp;
-import com.platon.rosettanet.admin.service.exception.ServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StopWatch;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import static com.platon.rosettanet.admin.grpc.constant.GrpcConstant.GRPC_SUCCESS_CODE;
 
@@ -58,6 +44,20 @@ public class LocalOrgRefreshTask {
             LocalOrgIdentityCache.setIdentityId(null);
             return;
         }
+        //### 1.刷新调度服务连接状态
+        boolean connect = yarnClient.connectScheduleServer(localOrg.getCarrierIp(), localOrg.getCarrierPort());
+        if(connect){
+            localOrg.setCarrierConnStatus(CarrierConnStatusEnum.ENABLED.getStatus());
+        } else {
+            localOrg.setCarrierConnStatus(CarrierConnStatusEnum.DISABLED.getStatus());
+        }
+
+        localOrgMapper.updateSelective(localOrg);
+        //刷新缓存
+        LocalOrgCache.setLocalOrgInfo(localOrg);
+        LocalOrgIdentityCache.setIdentityId(localOrg.getIdentityId());
+
+        //### 2.刷新调度服务状态和入网状态
         YarnGetNodeInfoResp nodeInfo = yarnClient.getNodeInfo(localOrg.getCarrierIp(), localOrg.getCarrierPort());
         if(nodeInfo.getStatus() != GRPC_SUCCESS_CODE){
             log.info("获取调度服务节点信息失败：" + nodeInfo.getMsg());
@@ -71,6 +71,7 @@ public class LocalOrgRefreshTask {
         } else {
             localOrg.setStatus(LocalOrgStatusEnum.LEAVE.getStatus());
         }
+
         localOrgMapper.updateSelective(localOrg);
         //刷新缓存
         LocalOrgCache.setLocalOrgInfo(localOrg);
