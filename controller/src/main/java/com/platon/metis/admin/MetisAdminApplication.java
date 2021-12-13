afcd0900e95e99c39443f195f41ddff4d8a19daf
bglmmz <lvxiaoyi@juzix.net>
2021-12-13 15:43:32 +0800

    init task scheduler pool size

--- controller/src/main/java/com/platon/metis/admin/MetisAdminApplication.java
+++ controller/src/main/java/com/platon/metis/admin/MetisAdminApplication.java
@@ -9,7 +9,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +39,13 @@ public class MetisAdminApplication {
         SpringApplication.run(MetisAdminApplication.class, args);
     }
 
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        return taskScheduler;
+    }
 
     /**
      * 应用启动后做一些操作
